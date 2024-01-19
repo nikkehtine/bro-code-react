67678adc1c2de9fd20b8c8be255572b717f4689e
nikkehtine <nikkehtine@int.pl>
2024-01-20 00:26:33 +0100

    Chapter 3 inline styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import Button from "./Button.jsx";
+
 function App() {
-    return <></>;
+    return <Button />;
 }
 
 export default App;
---
+++ src/Button.jsx
@@ -0,0 +1,13 @@
+function Button() {
+    const styles = {
+        backgroundColor: "hsl(200, 75%, 25%)",
+        color: "white",
+        padding: "0.5em 1em",
+        borderRadius: "0.5rem",
+        border: "none",
+        cursor: "pointer",
+    };
+
+    return <button style={styles}>Click me</button>;
+}
+export default Button;
